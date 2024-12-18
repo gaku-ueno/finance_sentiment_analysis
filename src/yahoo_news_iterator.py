4a52e4d991d332a8930f2ac4680c2914e90000b7
Gaku Ueno <gueno@ucsd.edu>
2024-12-18 08:46:32 -0800

    added to the iterator for the articles

--- src/yahoo_news_iterator.py
+++ src/yahoo_news_iterator.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+from tqdm import tqdm
 
 driver = webdriver.Chrome()
 
@@ -14,4 +16,35 @@ articles_container = WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "ul.stream-items.yf-1usaaz9"))
 )
 
-print('exists')
+i = 0
+article_count = 0
+target_article_count = 25
+
+print("initial article count: ", article_count)
+
+while article_count < target_article_count:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    articles = driver.find_elements(By.CSS_SELECTOR, "ul.stream-items.yf-1usaaz9 > li")
+    
+    article_count = len(articles)
+    i += 1
+    print(f"article count after {i} th iteration: ", article_count)
+
+valid_articles = 0
+ads = 0
+
+for article in tqdm(articles):
+
+    article_class = article.get_attribute("class")
+
+    #condition for if the article is an ad, skip it
+    if "ad-item" in article_class:
+        ads += 1
+        continue
+
+    valid_articles += 1
+
+    
+
+if input("Type 'quit' to finish session: ") == 'quit':
+    driver.quit()
